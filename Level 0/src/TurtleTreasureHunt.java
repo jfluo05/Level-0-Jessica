f40af1f7ecd14d29f2c6d617179addffd80f4c01
CactusCat <wjmorris2004@gmail.com>
2017-07-06 15:36:31 -0700

    We are making a Turtle Treasure Hunt.

--- Level 0/src/TurtleTreasureHunt.java
+++ Level 0/src/TurtleTreasureHunt.java
@@ -12,13 +12,13 @@ import org.teachingextensions.logo.Tortoise;
 
 /** Note: You will need the latest version of the TKP jar: http://school.wintrisstech.org/jars/TeachingKidsProgramming.jar **/
 
-public class TurtleTreasureHunt implements KeyListener {
+public class TurtleTreasureHunt implements KeyListener, KeyEventDispatcher {
 
 	int tortoiseSpeed = 5;
 
 	private void goUp() {
 		// 1. Make the tortoise move up the screen
-			Tortoise.move(10);
+			Tortoise.move(tortoiseSpeed);
 		
 	}
 
@@ -49,16 +49,26 @@ Tortoise.move(-10);
 		int tortoiseLocationY = Tortoise.getY();
 
 		// 5. Print out the variables for tortoiseLocationX and tortoiseLocationY
-System.out.println(tortoiseLocationX);
-System.out.println(tortoiseLocationY);
+System.out.println( "x"+ tortoiseLocationX);
+System.out.println("y"+ tortoiseLocationY);
 		// 6. If tortoise is at same location as the little girl,
 		// 			make a pop-up tell the Tortoise where to go next
+if(tortoiseLocationX==500 && tortoiseLocationY==300){
+	JOptionPane.showMessageDialog(null, "Doog boj! Won og ot eht yob dna ksa mih rof eht yek. Kcilc ecaps rof mih ot llet uoy erehw eht yek si (drawkcab)  ");
+	
+}
 
 		// 7. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
 
+	if(tortoiseLocationX==140 && tortoiseLocationY==290){
+		JOptionPane.showMessageDialog(null, "I swallowed the key and it is giving me a stomach ache. Press the space key to help me get it out.  ");}
+		
+		
 		
 	}
+
+	
 
 	private void go() {
 	
@@ -66,12 +76,18 @@ System.out.println(tortoiseLocationY);
 		 * If you want to use your own background, download the image you want, and change the following line to point to it like: new
 		 * ImageBackground("file:/Users/joonspoon/Desktop/dinosaurLand.jpg");
 		 */
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
+		
+		
+		Tortoise.penUp();
+		Tortoise.setSpeed(10);
 		Paintable backgroundImage = new ImageBackground("file:/Users/League/Google Drive/league-images/treasure_hunt.jpg");
 		Tortoise.getBackgroundWindow().addPaintable(backgroundImage);
 		Tortoise.getBackgroundWindow().addKeyListener(this);
 		Tortoise.penUp();
+	
 		Tortoise.getBackgroundWindow().getParent().addKeyListener(this);
-		JOptionPane.showMessageDialog(null, "Ask the little girl for help with your quest. Press the space bar to ask.");
+		JOptionPane.showMessageDialog(null, "Ask the little girl for help with your quest. Crawl on her head where her flower is, then press the space bar to ask.");
 	}
 
 	
@@ -99,13 +115,58 @@ System.out.println(tortoiseLocationY);
 				goDown();
 			else if (e.getKeyCode() == KeyEvent.VK_SPACE)
 				spaceBarWasPressed();
+			if (e.getKeyCode() == KeyEvent.VK_S)
+				sKeyWasPressed();
+			if (e.getKeyCode() == KeyEvent.VK_U)
+				uKeyWasPressed();
 			}
+	
+
+	private void uKeyWasPressed() {
+		int tortoiseLocationX = Tortoise.getX();
+		int tortoiseLocationY = Tortoise.getY();
+if (tortoiseLocationX==500 && tortoiseLocationY==300){
+JOptionPane.showMessageDialog(null, "You have unlocked the girl!");
+}
+	}
+	
+	private void sKeyWasPressed() {
+		int tortoiseLocationX = Tortoise.getX();
+		int tortoiseLocationY = Tortoise.getY();
+				if(tortoiseLocationX==480 && tortoiseLocationY==185){
+					JOptionPane.showMessageDialog(null, "Congratulations! You have the key! Go to the girl's head and press the u key to unlock the girl from the ropes.");
+				}
+				
+		JOptionPane.showMessageDialog(null, "Okay. It is out of my body now, but I don't know where it went. I am pretty sure it is inside the cannon.");
+	
+	}
 	
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			goRight();
+		else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			goLeft();
+		else if (e.getKeyCode() == KeyEvent.VK_UP)
+			goUp();
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			goDown();
+		else if (e.getKeyCode() == KeyEvent.VK_SPACE)
+			spaceBarWasPressed();
+		else if (e.getKeyCode() == KeyEvent.VK_S)
+			sKeyWasPressed();
+		
+
+		return false;
 	}
 }
 
